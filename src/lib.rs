30cd00ad2b838e18d78589372a471f4602bbe5df
Joshua Warner <joshuawarner32@gmail.com>
2020-11-29 15:31:45 -0800

    Remove unneeded feature declarations

--- src/lib.rs
+++ src/lib.rs
@@ -56,8 +56,6 @@
 //! # Logging
 //! This library uses the `log` crate to log errors during retries. Please see that create
 //! on methods on display such errors. If no logger is setup, nothing will be logged.
-#![feature(proc_macro_hygiene)]
-#![feature(stmt_expr_attributes)]
 pub mod client;
 mod dns;
 pub mod error;
